63f13d8209e9843cbaed420681282b5fc6e65083
Pothulakalyan <96769120+Pothulakalyan@users.noreply.github.com>
2022-06-30 20:18:41 +0530

    def rev(a):     su=0     while a>0:         rem=a%10         su=(su*10)+rem         a//=10     return su a=int(input()) arev=rev(a) if a**2==rev(arev**2):     print("True") else:     print("False")

--- Adam_number.py
+++ Adam_number.py
@@ -1,10 +1,13 @@
-x = input()
-a = int(x)**2
-y = x[::-1]
-b = int(y)**2
-c = str(b)
-d = c[::-1]
-if str(a)==d:
-    print(True)
+def rev(a):
+    su=0
+    while a>0:
+        rem=a%10
+        su=(su*10)+rem
+        a//=10
+    return su
+a=int(input())
+arev=rev(a)
+if a**2==rev(arev**2):
+    print("True")
 else:
-    print(False)
+    print("False")
