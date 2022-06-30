b0c472592c20a09e08dc3e9099ee65e7ab0094c1
Pothulakalyan <96769120+Pothulakalyan@users.noreply.github.com>
2022-06-30 20:11:38 +0530

    a,b=map(int,input().split()) hcf=1 for i in range(1,max(a,b)):     if a%i==0 and b%i==0:         hcf=i print(hcf)

--- GCD_or_HCF.py
+++ GCD_or_HCF.py
@@ -1,18 +1,6 @@
-x , y = map(int,input().split())
-l=[]
-m=[]
-h=[]
-for i in range(1,x+1):
-    if x%i==0:
-        l.append(i)
-for j in range(1,y+1):
-    if y%j==0:
-        m.append(j)
-for k in l:
-    for n in m:
-        if k==n:
-           h.append(k)
-if h!=0:
-    print(max(h))
-else:
-    print('-1')
+a,b=map(int,input().split())
+hcf=1
+for i in range(1,max(a,b)):
+    if a%i==0 and b%i==0:
+        hcf=i
+print(hcf)
