c4edee7748931c1725e7d4c172a975b20a5baa88
Pothulakalyan <96769120+Pothulakalyan@users.noreply.github.com>
2022-06-30 20:30:55 +0530

    a=int(input()) su=0 for i in range(1,a):     if a%i==0:         su+=i if su>a:     print("True") else:     print("False")

--- Abundant_Numbers.py
+++ Abundant_Numbers.py
@@ -1,9 +1,9 @@
-x = int(input())
-s=0
-for i in range(1,x):
-    if x%i==0:
-        s+=i
-if s>x:
+a=int(input())
+su=0
+for i in range(1,a):
+    if a%i==0:
+        su+=i
+if su>a:
     print("True")
 else:
     print("False")
